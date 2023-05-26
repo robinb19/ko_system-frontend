a279e5a4737fae49bceaf7e3a9ec3a4f2baad1ac
Robin Bühler <robinbuehler19@gmail.com>
2023-05-26 08:36:43 +0200

    inital commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,56 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { AuthConfig, OAuthModule, OAuthStorage } from 'angular-oauth2-oidc';
+import { environment } from 'src/environments/environment.development';
+import { HTTP_INTERCEPTORS, HttpClientXsrfModule } from '@angular/common/http';
+import { LocationStrategy, PathLocationStrategy } from '@angular/common';
+
+export const authConfig: AuthConfig = {
+  issuer: 'http://localhost:8080/realms/ILV',
+  requireHttps: false,
+  redirectUri: environment.frontendBaseUrl,
+  postLogoutRedirectUri: environment.frontendBaseUrl,
+  clientId: 'demoapp',
+  scope: 'openid profile roles offline_access',
+  responseType: 'code',
+  showDebugInformation: true,
+  requestAccessToken: true,
+  silentRefreshRedirectUri: window.location.origin + '/silent-refresh.html',
+  silentRefreshTimeout: 500,
+  clearHashAfterLogin: true,
+};
+
+export function storageFactory(): OAuthStorage {
+  return sessionStorage;
+}
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSlideToggleModule,
+    OAuthModule.forRoot({resourceServer: {sendAccessToken: true}}),
+    HttpClientXsrfModule.withOptions({
+      cookieName: 'XSRF-TOKEN',
+      headerName: 'X-XSRF-TOKEN'
+    }),
+  ],
+  providers: [{provide: AuthConfig, useValue: authConfig},
+    {provide: HTTP_INTERCEPTORS, useClass: HttpXSRFInterceptor, multi: true},
+    {
+      provide: OAuthStorage, useFactory: storageFactory
+    },
+    AppAuthGuard,
+    Location, {provide: LocationStrategy, useClass: PathLocationStrategy}],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  backendBaseUrl: 'http://localhost:9092/api/',
+  frontendBaseUrl: 'http://localhost:4200'
+};
